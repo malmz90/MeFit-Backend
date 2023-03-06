3c0510e8c537deedba57364ebbd396179adccf46
malmz90 <70096644+malmz90@users.noreply.github.com>
2023-03-06 14:07:47 +0100

    spring bot setup

---
+++ src/main/java/com/example/mefitbackend/MeFitBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.mefitbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeFitBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeFitBackendApplication.class, args);
+    }
+
+}
