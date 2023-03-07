786033d37a24b0a6335d671fa1a829dc3d6dc546
Amanda Ståhlberg <stahlberg.amanda@hotmail.com>
2023-03-07 14:58:47 +0100

    get all profiles and get profile by id

--- src/main/java/com/example/mefitbackend/models/Profile.java
+++ src/main/java/com/example/mefitbackend/models/Profile.java
@@ -1,10 +1,14 @@
 package com.example.mefitbackend.models;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
 @Entity
+@Getter
+@Setter
 public class Profile {
 
     @Id
---
+++ src/main/java/com/example/mefitbackend/services/ProfileService.java
@@ -0,0 +1,23 @@
+package com.example.mefitbackend.services;
+
+import com.example.mefitbackend.models.Profile;
+import com.example.mefitbackend.repositories.ProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProfileService {
+
+    @Autowired
+    private ProfileRepository profileRepository;
+
+    public List<Profile> getProfiles() {
+        return profileRepository.findAll();
+    }
+
+    public Profile getProfileById(Integer id) {
+        return profileRepository.findById(id).get();
+    }
+}
