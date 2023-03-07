f9e96ef7f35a33d9f03807a47b90c845c673bd32
Amanda Ståhlberg <stahlberg.amanda@hotmail.com>
2023-03-07 10:38:37 +0100

    add goal entity

---
+++ src/main/java/com/example/mefitbackend/models/Goal.java
@@ -0,0 +1,33 @@
+package com.example.mefitbackend.models;
+
+import com.fasterxml.jackson.annotation.JsonGetter;
+import jakarta.persistence.*;
+
+@Entity
+public class Goal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "start_date")
+    private String startDate;
+
+    @Column(name = "end_date")
+    private String endDate;
+
+    @Column(name = "achieved")
+    private boolean achieved;
+
+  /*  @ManyToOne
+    @JoinColumn(name = "program_id")
+    private Program program;*/
+
+    /*@ManyToOne
+    @JoinColumn(name = "profile_id")
+    private Profile profile;*/
+
+    /*@OneToMany(mappedBy = "workout")
+    List<WorkoutComplete> workoutsCompleted;
+    */
+
+}
