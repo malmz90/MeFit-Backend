b525210475929abc4ee51a19b1ef8d7f25db6fb8
malmz90 <70096644+malmz90@users.noreply.github.com>
2023-03-07 10:53:23 +0100

    Workout

---
+++ src/main/java/com/example/mefitbackend/models/Workout.java
@@ -0,0 +1,33 @@
+package com.example.mefitbackend.models;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Workout {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    @Column
+    private String name;
+    @Column
+    private String type;
+
+  /*  @ManyToMany
+    @JoinTable(
+            name = "program_workout",
+            joinColumns = {@JoinColumn(name = "workout_id")},
+            inverseJoinColumns = {@JoinColumn(name = "program_id")}
+    )
+    public List<Program> programs = new ArrayList<>();*/
+
+ /*   @ManyToMany
+    @JoinTable(
+            name = "workout_exercise",
+            joinColumns = {@JoinColumn(name = "workout_id")},
+            inverseJoinColumns = {@JoinColumn(name = "exercise_id")}
+    )
+    public List<Exercise> exercises = new ArrayList<>();*/
+/* @OneToMany(mappedBy = "workout")
+ List<WorkoutComplete> workoutsCompleted;*/
+    
+}
