b95fa53fbf91e8484badd5380241b80dc08f4c41
Amanda Ståhlberg <stahlberg.amanda@hotmail.com>
2023-03-07 11:01:40 +0100

    add exercise entity

---
+++ src/main/java/com/example/mefitbackend/models/Exercise.java
@@ -0,0 +1,44 @@
+package com.example.mefitbackend.models;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Exercise {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long Id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String description;
+
+    @Column(name = "upper_body")
+    private String upperBody;
+
+    @Column(name = "lower_body")
+    private String lowerBody;
+
+/*    @ManyToMany
+    @JoinTable(
+            name = "workout_exercise",
+            joinColumns = {@JoinColumn(name = "exercise_id")},
+            inverseJoinColumns = {@JoinColumn(name= "workout_id")}
+    )
+    public List<Workout> workouts = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(
+            name = "exercise_set",
+            joinColumns = {@JoinColumn(name = "exercise_id")},
+            inverseJoinColumns = {@JoinColumn(name = "set_id")}
+    )
+    public List<Set> sets = new ArrayList<>();*/
+
+
+}
