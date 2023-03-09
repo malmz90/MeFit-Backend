0d9422c7614d3510dad2c100dc7f8927fe10752a
Amanda Ståhlberg <stahlberg.amanda@hotmail.com>
2023-03-09 13:35:01 +0100

    add operation to profile

--- src/main/java/com/example/mefitbackend/controllers/ProfileController.java
+++ src/main/java/com/example/mefitbackend/controllers/ProfileController.java
@@ -4,6 +4,7 @@ import com.example.mefitbackend.models.Profile;
 import com.example.mefitbackend.repositories.UserRepository;
 import com.example.mefitbackend.services.ProfileService;
 import com.example.mefitbackend.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,17 +26,19 @@ public class ProfileController {
      * @return Iterable<Profile>
      */
 
-    // Get all profiles
+    @Operation(summary = "Get all profiles")
     @GetMapping("profiles")
     public List<Profile> getProfiles() {
         return profileService.getAll();
     }
 
+    @Operation(summary = "Get profile by ID")
     @GetMapping("{id}")
     public ResponseEntity<Profile> getProfileById(@PathVariable int id) {
         return ResponseEntity.ok(profileService.findById(id));
     }
 
+    @Operation(summary = "Create a new profile")
     @PostMapping()
     public ResponseEntity<Profile> addProfile(@RequestBody Profile profile) {
         Profile addProfile = profileService.add(profile);
@@ -43,6 +46,7 @@ public class ProfileController {
         return ResponseEntity.created(location).build();
     }
 
+    @Operation(summary = "Updating an existing profile by ID")
     @PatchMapping("{id}")
     public ResponseEntity<Profile> updateProfile(@RequestBody Profile profile, @PathVariable int id) {
         if(id != profile.getProfile_id())
@@ -51,6 +55,7 @@ public class ProfileController {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "Delete a profile by ID")
     @DeleteMapping("{id}")
     public ResponseEntity<Profile> deleteProfile(@PathVariable int id) {
         profileService.deleteById(id);
