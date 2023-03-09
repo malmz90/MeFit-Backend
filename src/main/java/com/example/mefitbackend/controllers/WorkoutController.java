eefb1f8e7df59cf923fb08631d3f013ac044fad6
Amanda Ståhlberg <stahlberg.amanda@hotmail.com>
2023-03-09 13:39:08 +0100

    add operations to workout

--- src/main/java/com/example/mefitbackend/controllers/WorkoutController.java
+++ src/main/java/com/example/mefitbackend/controllers/WorkoutController.java
@@ -4,6 +4,7 @@ import com.example.mefitbackend.models.Profile;
 import com.example.mefitbackend.models.User;
 import com.example.mefitbackend.models.Workout;
 import com.example.mefitbackend.services.WorkoutService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,11 +19,13 @@ public class WorkoutController {
     WorkoutService workoutService;
     private HttpStatus httpStatus;
 
+    @Operation(summary = "Get all workouts")
     @GetMapping()
     public List<Workout> getProfiles() {
         return workoutService.getWorkouts();
     }
 
+    @Operation(summary = "Get workout by ID")
     @GetMapping("/{id}")
     public ResponseEntity<Workout> getUser(@PathVariable int id) {
         HttpStatus status;
@@ -37,6 +40,7 @@ public class WorkoutController {
         }
     }
 
+    @Operation(summary = "Create a new workout")
     @PostMapping
     public ResponseEntity<Workout> add(@RequestBody Workout workout) {
         httpStatus = HttpStatus.FORBIDDEN;
@@ -50,6 +54,7 @@ public class WorkoutController {
         return new ResponseEntity<>(workout, httpStatus);
     }
 
+    @Operation(summary = "Update an existing workout by ID")
     @PatchMapping("{id}")
     public ResponseEntity<User> updateUser(@PathVariable int id, @RequestBody Workout workout) {
 
@@ -60,6 +65,7 @@ public class WorkoutController {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "Delete workout by ID")
     @DeleteMapping("{id}")
     public ResponseEntity<Workout> delete(@PathVariable int id) {
         workoutService.deleteById(id);
